59c44d9a2d23be32351ff8c2b0f5598889f9c4c3
arctusadem <bruno.cesar.salgado@terra.com.br>
2020-11-30 23:40:51 -0300

    Change of http status on exception handling

--- src/main/java/com/example/adapters/http/customer/CustomerControllerAdvice.java
+++ src/main/java/com/example/adapters/http/customer/CustomerControllerAdvice.java
@@ -1,6 +1,6 @@
 package com.example.adapters.http.customer;
 
-import com.example.exceptions.CustomerDuplicityException;
+import com.example.exceptions.CustomerConflictException;
 import com.example.exceptions.DataNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,7 +13,7 @@ import javax.validation.ConstraintViolationException;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.*;
 
 @ControllerAdvice
 public class CustomerControllerAdvice extends ResponseEntityExceptionHandler {
@@ -26,23 +26,22 @@ public class CustomerControllerAdvice extends ResponseEntityExceptionHandler {
                     violation.getPropertyPath() + ": " + violation.getMessage());
         }
 
-        ErrorResponse errorResponse =
-                new ErrorResponse(BAD_REQUEST, ex.getLocalizedMessage(), errors);
+        ErrorResponse errorResponse = new ErrorResponse(BAD_REQUEST, ex.getLocalizedMessage(), errors);
         return new ResponseEntity<>(errorResponse, errorResponse.getStatus());
     }
 
     @ExceptionHandler(DataNotFoundException.class)
     public ResponseEntity<Object> CustomerNotFoundExceptionHandler (DataNotFoundException ex, WebRequest request){
 
-        ErrorResponse errorResponse = new ErrorResponse(BAD_REQUEST, ex.getLocalizedMessage());
+        ErrorResponse errorResponse = new ErrorResponse(NOT_FOUND, ex.getLocalizedMessage());
 
         return new ResponseEntity<>(errorResponse, errorResponse.getStatus());
     }
 
-    @ExceptionHandler(CustomerDuplicityException.class)
-    public ResponseEntity<Object> CustomerDuplicityExceptionHandler (CustomerDuplicityException ex, WebRequest request){
+    @ExceptionHandler(CustomerConflictException.class)
+    public ResponseEntity<Object> CustomerDuplicityExceptionHandler (CustomerConflictException ex, WebRequest request){
 
-        ErrorResponse errorResponse = new ErrorResponse(BAD_REQUEST, ex.getLocalizedMessage());
+        ErrorResponse errorResponse = new ErrorResponse(CONFLICT, ex.getLocalizedMessage());
 
         return new ResponseEntity<>(errorResponse, errorResponse.getStatus());
     }
